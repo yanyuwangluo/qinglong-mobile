8fc095b0966139add98d7d345f22b2e5709b94cb
chenfengshf <chenfengshf@hotmail.com>
2021-10-11 15:30:44 +0800

    push

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 : for push to github
-git config --global https.proxy http://127.0.0.1:1080
+git config --global https.proxy https://127.0.0.1:10809
 timeout /T 5 /NOBREAK
 git push
 git config --global --unset https.proxy
